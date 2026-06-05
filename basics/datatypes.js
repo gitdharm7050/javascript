164b2a4483b4a8669266e935fa5d9bb69caf81af
gitdharm7050 <browsedharm@gmail.com>
2026-06-05 21:57:30 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict ";           //treat all codes of js as newer version
+
+//alert(3+3)               we are using node js not browser
+//alert(dharmveer)         this alert is supported in console of browser
+
+console.log("dharmver")    
+
+
+// two documentations we can refer mdn and tc39(its original)
+
+
+let name ="dharm"       //when we write something in quetes its string. prefer to write strings in double quotes
+
+let age =18              //without qoutes its integer   its range 2 to power 53.
+                         //bigint for large numbers
+
+let isLogin=false       // boolean => true or false
+
+//null => standalone value
+//undefined => no value assigned yet
+//symbol = uniqueness ke liye use hota hai
+//object
+
+
+console.log(typeof "dharm")    // to know or get datatype
+console.log(typeof age)
+
+console.log(typeof null)        // object datatype hai
+
+console.log(typeof undefined)
